9f3fa9d2b341fd310f0bdccfc6998930d40551d5
DESKTOP-071711U\cuonghx2709 <cuonghx2709@gmail.com>
2017-09-03 23:13:00 +0700

    cuonghx
    added

---
+++ app/src/main/java/com/example/cuonghx2709/calculator/MainActivity.java
@@ -0,0 +1,315 @@
+package com.example.cuonghx2709.calculator;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = MainActivity.class.getName();
+    private Button button_0;
+    private Button button_1;
+    private Button button_2;
+    private Button button_3;
+    private Button button_4;
+    private Button button_5;
+    private Button button_6;
+    private Button button_7;
+    private Button button_8;
+    private Button button_9;
+    private RelativeLayout relaytiveLayout_x;
+    private RelativeLayout relativeLayout_add;
+    private RelativeLayout relaytiveLayout_subtract;
+    private RelativeLayout relaytiveLayout_division;
+
+    private Button button_del;
+    private Button button_final;
+    private Button button_change;
+    private Button button_percent;
+    private Button button_;
+
+    private TextView textView;
+
+    private double number = 0;
+    private double cachNumber = 0;
+    private int status = 0;
+    private boolean change = false;
+    private boolean error;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+
+    private void init() {
+        button_0 = (Button) findViewById(R.id.bt_0);
+        button_1 = (Button) findViewById(R.id.bt_1);
+        button_2 = (Button) findViewById(R.id.bt_2);
+        button_3 = (Button) findViewById(R.id.bt_3);
+        button_4 = (Button) findViewById(R.id.bt_4);
+        button_5 = (Button) findViewById(R.id.bt_5);
+        button_6 = (Button) findViewById(R.id.bt_6);
+        button_7 = (Button) findViewById(R.id.bt_7);
+        button_8 = (Button) findViewById(R.id.bt_8);
+        button_9 = (Button) findViewById(R.id.bt_9);
+        relaytiveLayout_x = (RelativeLayout) findViewById(R.id.rl_x);
+        relativeLayout_add = (RelativeLayout) findViewById(R.id.rl_add);
+        relaytiveLayout_subtract = (RelativeLayout) findViewById(R.id.rl_subtract);
+        relaytiveLayout_division = (RelativeLayout) findViewById(R.id.rl_division);
+        button_del = (Button) findViewById(R.id.bt_del);
+        button_final = (Button) findViewById(R.id.bt_final);
+        button_change = (Button) findViewById(R.id.bt_change);
+        button_percent = (Button) findViewById(R.id.bt_percent);
+        button_ = (Button) findViewById(R.id.bt_);
+
+        button_0.setOnClickListener(this);
+        button_1.setOnClickListener(this);
+        button_2.setOnClickListener(this);
+        button_3.setOnClickListener(this);
+        button_4.setOnClickListener(this);
+        button_5.setOnClickListener(this);
+        button_6.setOnClickListener(this);
+        button_7.setOnClickListener(this);
+        button_8.setOnClickListener(this);
+        button_9.setOnClickListener(this);
+        button_.setOnClickListener(this);
+        relaytiveLayout_x.setOnClickListener(this);
+        relativeLayout_add.setOnClickListener(this);
+        relaytiveLayout_subtract.setOnClickListener(this);
+        relaytiveLayout_division.setOnClickListener(this);
+        button_del.setOnClickListener(this);
+        button_final.setOnClickListener(this);
+        button_change.setOnClickListener(this);
+        button_percent.setOnClickListener(this);
+
+        textView = (TextView) findViewById(R.id.tv_number);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.bt_0:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 ;
+                break;
+            case R.id.bt_1:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number  = 0;
+
+                }
+                number = number * 10 + 1;
+                break;
+            case R.id.bt_2:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number = number * 10 + 2;
+                break;
+            case R.id.bt_3:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number = number * 10 + 3;
+                break;
+            case R.id.bt_4:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 4;
+                break;
+            case R.id.bt_5:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 5;
+                break;
+            case R.id.bt_6:
+                if (change){
+                    number = 0;
+                    change =  false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 6;
+                break;
+            case R.id.bt_7:
+                if (change){
+                number = 0;
+                change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 7;
+                break;
+            case R.id.bt_8:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 8;
+                break;
+            case R.id.bt_9:
+                if (change){
+                    number = 0;
+                    change = false;
+                }else if (error){
+                    error = false;
+                    number = 0;
+                }
+                number  = number * 10 + 9;
+                break;
+            case R.id.rl_add:
+                count(1);
+                break;
+            case R.id.rl_subtract:
+                count(2);
+                break;
+            case R.id.rl_x:
+                count(3);
+                break;
+            case R.id.rl_division:
+                count(4);
+                break;
+            case R.id.bt_del:
+                error = false;
+                number = 0;
+                cachNumber = 0;
+                status = 0;
+                break;
+            case R.id.bt_final:
+                if (status != 0)
+                count(0);
+                break;
+            case R.id.bt_change:
+                number = -number;
+                break;
+            case R.id.bt_percent:
+                number = number / 100 ;
+                cachNumber = number;
+                break;
+            case R.id.bt_:
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                break;
+        }
+
+//        changeColor();
+
+
+        if (error){
+            textView.setText("error");
+        }else {
+            int check = (int) number;
+
+            if (number - check == 0)
+                textView.setText(String.format("%.0f", number));
+            else{
+                textView.setText(String.format("%.2f", number));
+            }
+        }
+
+    }
+
+//    private void changeColor() {
+//        switch (status){
+//            case 1:
+//                relativeLayout_add.setBackgroundColor(Color.GRAY);
+//                break;
+//            case 2:
+//                relaytiveLayout_subtract.setBackgroundColor(Color.GRAY);
+//                break;
+//            case 3:
+//                relaytiveLayout_x.setBackgroundColor(Color.GRAY);
+//                break;
+//            case 4:
+//                relaytiveLayout_division.setBackgroundColor(Color.GRAY);
+//                break;
+//            default:
+//                relativeLayout_add.setBackgroundColor(Color.WHITE);
+//                relaytiveLayout_subtract.setBackgroundColor(Color.WHITE);
+//                relaytiveLayout_x.setBackgroundColor(Color.WHITE);
+//                relaytiveLayout_division.setBackgroundColor(Color.WHITE);
+//                break;
+//        }
+//    }
+
+
+    private void  count(int n){
+        if (status == 0){
+            cachNumber = number;
+            number = 0;
+            status = n;
+        }else {
+            change = true;
+            if (status == 1){
+                number = number + cachNumber;
+                cachNumber = number;
+                status = n;
+            }
+            if (status == 2){
+                number = cachNumber - number;
+                cachNumber = number;
+                status = n;
+            }
+            if (status == 3){
+                number = cachNumber * number;
+                cachNumber = number;
+                status = n;
+            }
+            if (status == 4){
+                if (number != 0){
+                    number = cachNumber / number;
+                    cachNumber = number;
+                    status = n;
+                }else {
+                    error = true;
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/cuonghx2709/calculator/Status.java
@@ -0,0 +1,15 @@
+package com.example.cuonghx2709.calculator;
+
+/**
+ * Created by cuonghx2709 on 8/31/2017.
+ */
+
+public enum Status {
+    normal,
+    add,
+    division,
+    subtract,
+    multiplication;
+}
+
+
